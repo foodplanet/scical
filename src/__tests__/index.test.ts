c7f7ae8eea6a99f411ca517263dea470fba982d5
Tom Richter <tomrichter99@gmail.com>
2021-03-12 13:40:33 +0100

    add PrecisionNumber with constructor and toString

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,30 @@
 import { PrecisionNumber } from '../index'
 
-test('PrecisionNumber', () => {
-  expect(PrecisionNumber(3, 4)).toBe('3#4')
+test('constructor', () => {
+  expect(new PrecisionNumber(23, 0).significand).toEqual(23)
+  expect(new PrecisionNumber(23, 0).exponent).toEqual(0)
+  expect(new PrecisionNumber(23, 0).precision).toEqual(2)
+  expect(new PrecisionNumber(-8, 1).significand).toEqual(-8)
+  expect(new PrecisionNumber(-8, 1).exponent).toEqual(1)
+  expect(new PrecisionNumber(-8, 1).precision).toEqual(1)
+  expect(new PrecisionNumber(523, -24, 2).significand).toEqual(523)
+  expect(new PrecisionNumber(523, -24, 2).exponent).toEqual(-24)
+  expect(new PrecisionNumber(523, -24, 2).precision).toEqual(2)
+  expect(() => new PrecisionNumber(67, 0, 3)).toThrow(RangeError)
+  expect(() => new PrecisionNumber(-9, 0, 0)).toThrow(RangeError)
+  expect(() => new PrecisionNumber(0, 0, -1)).toThrow(RangeError)
+})
+
+test('toString', () => {
+  expect(new PrecisionNumber(23, 0).toString()).toEqual('23E0')
+  expect(new PrecisionNumber(826, 1).toString()).toEqual('826E1')
+  expect(new PrecisionNumber(42, 0, 1).toString()).toEqual('4E0')
+  expect(new PrecisionNumber(178, 2, 1).toString()).toEqual('2E2')
+  expect(new PrecisionNumber(995, 0, 2).toString()).toEqual('100E0')
+  expect(new PrecisionNumber(0, 0).toString()).toEqual('0E0')
+  expect(new PrecisionNumber(0, -2).toString()).toEqual('0E-2')
+  expect(new PrecisionNumber(-89, 1).toString()).toEqual('-89E1')
+  expect(new PrecisionNumber(-7123, 0, 2).toString()).toEqual('-71E0')
+  expect(new PrecisionNumber(-655, 0, 2).toString()).toEqual('-66E0')
+  expect(new PrecisionNumber(-999, 0, 2).toString()).toEqual('-100E0')
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-export const PrecisionNumber = (a: number, b: number) => `${a}#${b}`
+export class PrecisionNumber {
+  private _significand: number
+  private _exponent: number
+  private _precision: number
+
+  constructor(significand: number, exponent: number, precision?: number) {
+    this._significand = significand
+    this._exponent = exponent
+    if (
+      typeof precision === 'undefined' ||
+      (precision > 0 && precision <= significand.toString().length)
+    ) {
+      this._precision = precision || Math.abs(significand).toString().length
+    } else {
+      throw new RangeError(
+        'The precision must be greater than 0 and smaller or equal than the number of digits of the significand.',
+      )
+    }
+  }
+
+  get significand(): number {
+    return this._significand
+  }
+
+  set significand(significand: number) {
+    this._significand = significand
+  }
+
+  get exponent(): number {
+    return this._exponent
+  }
+
+  set exponent(exponent: number) {
+    this._exponent = exponent
+  }
+
+  get precision(): number {
+    return this._precision
+  }
+
+  set precision(precision: number) {
+    this._precision = precision
+  }
+
+  toString(): string {
+    let output = this.significand < 0 ? '-' : ''
+
+    const absoluteSignificand = Math.abs(this.significand).toString()
+    output += (
+      parseInt(absoluteSignificand.slice(0, this.precision), 10) +
+      (parseInt(absoluteSignificand[this.precision], 10) > 4 ? 1 : 0)
+    ).toString()
+
+    output += 'E' + this._exponent.toString()
+
+    return output
+  }
+}
