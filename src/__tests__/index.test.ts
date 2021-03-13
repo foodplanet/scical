9933b0756f34885ee7bd5246a42b97b42e3074ac
Tom Richter <tomrichter99@gmail.com>
2021-03-13 15:19:17 +0100

    refactor significand

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,42 +1,110 @@
 import { PrecisionNumber } from '../index'
 
 test('constructor', () => {
-  expect(new PrecisionNumber(23, 0).significand).toEqual(23)
-  expect(new PrecisionNumber(23, 0).exponent).toEqual(0)
-  expect(new PrecisionNumber(23, 0).precision).toEqual(2)
-  expect(new PrecisionNumber(-8, 1).significand).toEqual(-8)
-  expect(new PrecisionNumber(-8, 1).exponent).toEqual(1)
-  expect(new PrecisionNumber(-8, 1).precision).toEqual(1)
-  expect(new PrecisionNumber(523, -24, 2).significand).toEqual(523)
-  expect(new PrecisionNumber(523, -24, 2).exponent).toEqual(-24)
-  expect(new PrecisionNumber(523, -24, 2).precision).toEqual(2)
-  expect(() => new PrecisionNumber(67, 0, 3)).toThrow(RangeError)
-  expect(() => new PrecisionNumber(-9, 0, 0)).toThrow(RangeError)
-  expect(() => new PrecisionNumber(0, 0, -1)).toThrow(RangeError)
+  expect(new PrecisionNumber('23', false, 0).significand).toEqual('23')
+  expect(new PrecisionNumber('23', false, 0).isSignificandNegative).toEqual(false)
+  expect(new PrecisionNumber('23', false, 0).exponent).toEqual(0)
+  expect(new PrecisionNumber('23', false, 0).precision).toEqual(2)
+  expect(new PrecisionNumber('8', true, 1).significand).toEqual('8')
+  expect(new PrecisionNumber('8', true, 1).isSignificandNegative).toEqual(true)
+  expect(new PrecisionNumber('8', true, 1).exponent).toEqual(1)
+  expect(new PrecisionNumber('8', true, 1).precision).toEqual(1)
+  expect(new PrecisionNumber('523', false, -24, 2).significand).toEqual("523")
+  expect(new PrecisionNumber('523', false, -24, 2).isSignificandNegative).toEqual(false)
+  expect(new PrecisionNumber('523', false, -24, 2).exponent).toEqual(-24)
+  expect(new PrecisionNumber('523', false, -24, 2).precision).toEqual(2)
+
+  const num1= new PrecisionNumber('012', false, -3, 2)
+  expect(num1.significand).toEqual('012')
+  expect(num1.isSignificandNegative).toEqual(false)
+  expect(num1.exponent).toEqual(-3)
+  expect(num1.precision).toEqual(2)
+
+  const num2= new PrecisionNumber('012', false, 0)
+  expect(num2.significand).toEqual('12')
+  expect(num2.isSignificandNegative).toEqual(false)
+  expect(num2.exponent).toEqual(0)
+  expect(num2.precision).toEqual(2)
+
+  const num3= new PrecisionNumber('00', true, 0)
+  expect(num3.significand).toEqual('0')
+  expect(num3.isSignificandNegative).toEqual(false)
+  expect(num3.exponent).toEqual(0)
+  expect(num3.precision).toEqual(1)
+
+  const num4= new PrecisionNumber('000', false, 0, 1)
+  expect(num4.significand).toEqual('000')
+  expect(num4.isSignificandNegative).toEqual(false)
+  expect(num4.exponent).toEqual(0)
+  expect(num4.precision).toEqual(1)
+
+  expect(() => new PrecisionNumber('67', false, 0, 3)).toThrow(RangeError)
+  expect(() => new PrecisionNumber('9', true, 0, 0)).toThrow(RangeError)
+  expect(() => new PrecisionNumber('0', false, 0, -1)).toThrow(RangeError)
+
+  expect(() => new PrecisionNumber('-5', false, 0)).toThrow(Error)
+  expect(() => new PrecisionNumber('-5', true, 0)).toThrow(Error)
+  expect(() => new PrecisionNumber('-0', true, 0)).toThrow(Error)
+  expect(() => new PrecisionNumber('0.23', false, 0)).toThrow(Error)
 })
 
 test('toString', () => {
-  expect(new PrecisionNumber(23, 0).toString()).toEqual('23E0')
-  expect(new PrecisionNumber(826, 1).toString()).toEqual('826E1')
-  expect(new PrecisionNumber(42, 0, 1).toString()).toEqual('4E0')
-  expect(new PrecisionNumber(178, 2, 1).toString()).toEqual('2E2')
-  expect(new PrecisionNumber(995, 0, 2).toString()).toEqual('100E0')
-  expect(new PrecisionNumber(0, 0).toString()).toEqual('0E0')
-  expect(new PrecisionNumber(0, -2).toString()).toEqual('0E-2')
-  expect(new PrecisionNumber(-89, 1).toString()).toEqual('-89E1')
-  expect(new PrecisionNumber(-7123, 0, 2).toString()).toEqual('-71E0')
-  expect(new PrecisionNumber(-655, 0, 2).toString()).toEqual('-66E0')
-  expect(new PrecisionNumber(-999, 0, 2).toString()).toEqual('-100E0')
+  expect(new PrecisionNumber('23', false, 0).toString()).toEqual('23E0')
+  expect(new PrecisionNumber('826', false, 1).toString()).toEqual('826E1')
+  expect(new PrecisionNumber('42',false, 0, 1).toString()).toEqual('4E0')
+  expect(new PrecisionNumber('178',false, 2, 1).toString()).toEqual('2E2')
+  expect(new PrecisionNumber('995', false, 0, 2).toString()).toEqual('100E0')
+  expect(new PrecisionNumber('0',false, 0).toString()).toEqual('0E0')
+  expect(new PrecisionNumber('0', false, -2).toString()).toEqual('0E-2')
+  expect(new PrecisionNumber('89', true, 1).toString()).toEqual('-89E1')
+  expect(new PrecisionNumber('7123', true, 0, 2).toString()).toEqual('-71E0')
+  expect(new PrecisionNumber('655', true, 0, 2).toString()).toEqual('-66E0')
+  expect(new PrecisionNumber('999', true, 0, 2).toString()).toEqual('-100E0')
+  expect(new PrecisionNumber('09', false, 0, 1).toString()).toEqual('1E0')
+  expect(new PrecisionNumber('0003', true, 0, 3).toString()).toEqual('0E0')
 })
 
 test('fromString', () => {
-  expect(PrecisionNumber.fromString('23E1')).toEqual(new PrecisionNumber(23, 1))
+  expect(PrecisionNumber.fromString('23E1')).toEqual(new PrecisionNumber('23', false, 1))
   expect(PrecisionNumber.fromString('-892E-31')).toEqual(
-    new PrecisionNumber(-892, -31),
+    new PrecisionNumber('892', true, -31),
   )
-  expect(PrecisionNumber.fromString('0E0')).toEqual(new PrecisionNumber(0, 0))
-  expect(PrecisionNumber.fromString('-0E-0')).toEqual(new PrecisionNumber(0, 0))
+  expect(PrecisionNumber.fromString('0E0')).toEqual(new PrecisionNumber('0', false, 0))
+  expect(PrecisionNumber.fromString('-0E-0')).toEqual(new PrecisionNumber('0', false, 0))
   expect(() => PrecisionNumber.fromString('4e-2')).toThrow(Error)
   expect(() => PrecisionNumber.fromString('4E+2')).toThrow(Error)
   expect(() => PrecisionNumber.fromString('E55')).toThrow(Error)
 })
+
+
+// test.each([
+//   [23, 2, 2, 5, -1, 1, 23005, -1, 2],
+//   [-3, 0, 1, 5, 0, 1, 2, 0, 1],
+//   [999, -2, 2, 999, -1, 3, 10989, -2, 4],
+//   [1, 3, 1, 9, 2, 1, 19, 2, 1],
+//   // both significands are zero
+//   [0, 1, 1, 0, 0, 1, 0, 1, 1],
+//   [0, 0, 1, 0, 0, 1, 0, 0, 1],
+//   [0, -4, 1, 0, -4, 1, 0, -4, 1],
+//   [0, -11, 1, 0, 10, 1, 0, 10, 1],
+//   // one significand is zero
+//   [1, 0, 1, 0, 0, 1, 1, 0, 1],
+//   [45, 0, 2, 0, 1, 1, 45, 0, 1],
+// ])(
+//   '(%i, %i, %i).add((%i, %i, %i))',
+//   (
+//     significand1,
+//     exponent1,
+//     precision1,
+//     significand2,
+//     exponent2,
+//     precision2,
+//     significandRes,
+//     exponentRes,
+//     precisionRes
+//   ) => {
+//     const num1 = new PrecisionNumber(significand1, exponent1, precision1)
+//     const num2 = new PrecisionNumber(significand2, exponent2, precision2)
+//     expect(num1.add(num2)).toEqual(new PrecisionNumber(significandRes, exponentRes, precisionRes))
+//   }
+// )
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,60 @@
 export class PrecisionNumber {
-  private _significand: number
-  private _exponent: number
-  private _precision: number
+  private _significand = '0'
+  private _isSignificandNegative = false
+  private _exponent = 0
+  private _precision = 1
 
-  constructor(significand: number, exponent: number, precision?: number) {
-    this._significand = significand
-    this._exponent = exponent
-    if (
-      typeof precision === 'undefined' ||
-      (precision > 0 && precision <= significand.toString().length)
-    ) {
-      this._precision = precision || Math.abs(significand).toString().length
+  constructor(
+    significand: string,
+    isSignificandNegative: boolean,
+    exponent: number,
+    precision?: number,
+  ) {
+    // significand has to be set before exponent
+    this.significand = significand
+    this.isSignificandNegative = isSignificandNegative
+    this.exponent = exponent
+    if (typeof precision === 'undefined') {
+      this.precision = this.significand.length
     } else {
-      throw new RangeError(
-        'The precision must be greater than 0 and smaller or equal than the number of digits of the significand.',
-      )
+      this.precision = precision
     }
   }
 
   static fromString(rawNumber: string): PrecisionNumber {
     if (rawNumber.match(/^-?\d+E-?\d+$/)) {
-      const [rawSignificand, rawExponent] = rawNumber.split('E')
-      let significand = parseInt(rawSignificand, 10)
+      const [significand, rawExponent] = rawNumber.split('E')
       let exponent = parseInt(rawExponent, 10)
 
-      // make sure significand and exponent are not -0, should be done in setter
-      significand = significand === 0 ? 0 : significand
-      exponent = exponent === 0 ? 0 : exponent
-
-      return new this(significand, exponent)
+      if (significand[0] === '-') {
+        return new this(significand.slice(1), true, exponent)
+      } else {
+        return new this(significand, false, exponent)
+      }
     } else {
-      throw new Error('The string was invalid.')
+      throw new Error('The provided string is invalid.')
     }
   }
 
-  get significand(): number {
+  get significand(): string {
     return this._significand
   }
 
-  set significand(significand: number) {
-    this._significand = significand
+  set significand(significand: string) {
+    if (significand.match(/^\d+$/)) {
+      this._significand = significand
+    } else {
+      throw new Error('The significand contains illegal characters.')
+    }
+  }
+
+  get isSignificandNegative(): boolean {
+    return this._isSignificandNegative
+  }
+
+  set isSignificandNegative(isSignificandNegative: boolean) {
+    this._isSignificandNegative = isSignificandNegative
+    this.makeZeroSignificandPositive()
   }
 
   get exponent(): number {
@@ -47,7 +62,10 @@ export class PrecisionNumber {
   }
 
   set exponent(exponent: number) {
-    this._exponent = exponent
+    // ensure that exponent is not -0
+    this._exponent = exponent === 0 ? 0 : exponent
+    this.removeUnnecessaryLeadingZeros()
+    this.makeZeroSignificandPositive()
   }
 
   get precision(): number {
@@ -55,20 +73,90 @@ export class PrecisionNumber {
   }
 
   set precision(precision: number) {
+    if (precision > 0 && precision <= this.significand.length) {
+      this._precision = precision || this.significand.toString().length
+    } else {
+      throw new RangeError(
+        'The precision must be greater than 0 and smaller or equal than the number of digits of the significand.',
+      )
+    }
     this._precision = precision
+    this.removeUnnecessaryLeadingZeros()
+    this.makeZeroSignificandPositive()
+  }
+
+  private makeZeroSignificandPositive(): void {
+    if (this.significand === '0') {
+      this._isSignificandNegative = false
+    }
+  }
+
+  private removeUnnecessaryLeadingZeros(): void {
+    if (this.significand.length === this.precision) {
+      let numOfLeadingZeros = 0
+      for (const char of this.significand.slice(0, -1)) {
+        if (char === '0') {
+          numOfLeadingZeros++
+        } else {
+          break
+        }
+      }
+      this._significand = this.significand.slice(numOfLeadingZeros)
+      this._precision = this.significand.length
+    }
   }
 
   toString(): string {
-    let output = this.significand < 0 ? '-' : ''
-
-    const absoluteSignificand = Math.abs(this.significand).toString()
-    output += (
-      parseInt(absoluteSignificand.slice(0, this.precision), 10) +
-      (parseInt(absoluteSignificand[this.precision], 10) > 4 ? 1 : 0)
+    const significand = (
+      parseInt(this.significand.slice(0, this.precision), 10) +
+      (parseInt(this.significand[this.precision], 10) > 4 ? 1 : 0)
     ).toString()
 
-    output += 'E' + this._exponent.toString()
+    const sign = this.isSignificandNegative && significand !== '0' ? '-' : ''
 
-    return output
+    return sign + significand + 'E' + this.exponent.toString()
   }
+
+  //   add(num: PrecisionNumber): PrecisionNumber {
+  //     // special case if both significands are zero because padding does not work with: significand2 *= 10 ** padding bcs the number of digits doesn't increase
+  //     if (this.significand === 0 && num.significand === 0) {
+  //       return new PrecisionNumber(0, Math.max(this.exponent, num.exponent), 1)
+  //     }
+  //     let paddedSignificand = 0
+  //     let significand1 = this._significand
+  //     let significand2 = num.significand
+  //     let significand1InsignificantDigits = 0
+  //     let significand2InsignificantDigits = 0
+  //     let padding = 0
+
+  //     // padd the shorter significand with zeros to the same length as the longer one
+  //     if (this._exponent > num.exponent) {
+  //       paddedSignificand = 1
+  //       padding = this._exponent - num.exponent
+  //       significand1InsignificantDigits =
+  //         Math.abs(significand1).toString().length - this._precision + padding
+  //       significand2InsignificantDigits =
+  //         Math.abs(significand2).toString().length - num.precision
+  //       significand1 *= 10 ** padding
+  //     } else if (num.exponent > this._exponent) {
+  //       paddedSignificand = 2
+  //       padding = num.exponent - this._exponent
+  //       significand1InsignificantDigits =
+  //         Math.abs(significand1).toString().length - this._precision
+  //       significand2InsignificantDigits =
+  //         Math.abs(significand2).toString().length - num.precision + padding
+  //       significand2 *= 10 ** padding
+  //     }
+  //     const newSignificand = significand1 + significand2
+  //     // new exponent is the one we haven't padded
+  //     const newExponent = paddedSignificand === 2 ? this._exponent : num.exponent
+  //     const newPrecision =
+  //       Math.abs(newSignificand).toString().length -
+  //         Math.max(
+  //           significand1InsignificantDigits,
+  //           significand2InsignificantDigits,
+  //         ) || 1
+  //     console.log(newExponent, paddedSignificand)
+  //     return new PrecisionNumber(newSignificand, newExponent, newPrecision)
+  //   }
 }
